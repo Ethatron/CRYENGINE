b2d9fe63adb51cb60ad928b2da39512c9f963b23
Ethatron <niels@paradice-insight.us>
2018-01-17 22:47:57 +0100

    - added DX11 utility files

---
+++ Code/CryEngine/RenderDll/XRenderD3D9/DX11/CryDX11.hpp
@@ -0,0 +1,17 @@
+// Copyright 2001-2017 Crytek GmbH / Crytek Group. All rights reserved. 
+
+#pragma once
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+HRESULT WINAPI DX11CreateDevice(
+  IDXGIAdapter* pAdapter,
+  D3D_DRIVER_TYPE DriverType,
+  HMODULE Software,
+  UINT Flags,
+  CONST D3D_FEATURE_LEVEL* pFeatureLevels,
+  UINT FeatureLevels,
+  UINT SDKVersion,
+  ID3D11Device** ppDevice,
+  D3D_FEATURE_LEVEL* pFeatureLevel,
+  ID3D11DeviceContext** ppImmediateContext);
